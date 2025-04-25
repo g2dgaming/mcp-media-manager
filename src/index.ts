72a08da51743c595a7ee1216bb02b71fe0cc3e9f
samarth <srivastavasamarth39@gmail.com>
2025-04-25 22:01:38 +0530

    improved movie data

--- src/index.ts
+++ src/index.ts
@@ -54,42 +54,7 @@ type Movie = {
   title: string;
   [key: string]: any;
 };
-function levenshtein(a: string, b: string): number {
-  const matrix: number[][] = Array.from({ length: a.length + 1 }, (_, i) =>
-    Array.from({ length: b.length + 1 }, (_, j) => (i === 0 ? j : j === 0 ? i : 0))
-  );
 
-  for (let i = 1; i <= a.length; i++) {
-    for (let j = 1; j <= b.length; j++) {
-      if (a[i - 1].toLowerCase() === b[j - 1].toLowerCase()) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      } else {
-        matrix[i][j] = Math.min(
-          matrix[i - 1][j] + 1,
-          matrix[i][j - 1] + 1,
-          matrix[i - 1][j - 1] + 1
-        );
-      }
-    }
-  }
-
-  return matrix[a.length][b.length];
-}
-
-function getClosestTitleMatch(results: Movie[], targetTitle: string): object {
-  let closest: Movie | null = null;
-  let smallestDistance = Infinity;
-
-  for (const movie of results) {
-    const distance = levenshtein(movie.title, targetTitle);
-    if (distance < smallestDistance) {
-      smallestDistance = distance;
-      closest = movie;
-    }
-  }
-
-  return closest;
-}
 
 const server = new Server(
   {
